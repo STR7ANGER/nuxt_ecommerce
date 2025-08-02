1d6e7edad6fefb525742f249d51e3396758788b3
Aditya <adityamaurya.2807@gmail.com>
2025-08-02 18:00:44 +0530

    setup

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
+  <div class="fixed Z-[-1] bg-black h-[100vh]">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  css: ["@/assets/css/tailwind.css"],
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/icon",
+    "nuxt-lodash",
+    "@pinia/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@nuxtjs/tailwindcss",
+    // "@nuxtjs/supabase",
+  ],
+  runtimeConfig: {
+    public: {
+      stripePk: process.env.STRIPE_PK_KEY,
+    },
+  },
+  app: {
+    head: {
+      script: [{ src: "https://js.stripe.com/v3/", defer: true }],
+    },
+  },
+});
